be54566a275f3c2b826a46ad658da79f185ba103
BAD <soccerskin87@gmail.com>
2022-09-25 10:29:12 +0100

    Added lcm to old site

--- oldsite/javascript/hcf-lcm.js
+++ oldsite/javascript/hcf-lcm.js
@@ -1,10 +1,22 @@
-const lm = document.getElementById("outputlm")
-const hcf = document.getElementById("outputhf")
 function calculate(){
     // Get value with document.getElementById("id").value
-    
-    hcf.innerHTML = "output answer hch"
-    lm.innerHTML = "output answer lcm"
+    let firstone = document.getElementById("hfc").value
+    let lastone = document.getElementById("lcm").value
+    document.getElementById("outputhf").innerHTML = "HFC: "+hcf(firstone, lastone)
+    document.getElementById("outputlm").innerHTML = "LCM: " +firstone*lastone/hcf(firstone,lastone)
+}
+function hcf(a,b){
+    if(a>b){
+        [a,b] = [b,a]
+    }
+    while(a!=0 && b!=0){
+        [a,b] = [a,b-a]    
+        if(a>b){
+            [a,b] = [b,a]
+        }
+    }
+    console.log(a,b)
+    return Math.max(a,b);
 }
 
 
